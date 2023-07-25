505a689075b5d4c6469a7c07a864335c786a5aff
Omochice <mochice.mls.ntl@gmail.com>
2023-07-25 10:10:56 +0900

    :package: rename some filenames

--- mod.ts
+++ mod.ts
@@ -1,38 +1,13 @@
 import stylelint from "npm:stylelint@15.10.2";
-import { resolve, toFileUrl } from "https://deno.land/std@0.195.0/path/mod.ts";
-import {
-  Command,
-  EnumType,
-} from "https://deno.land/x/cliffy@v1.0.0-rc.2/command/mod.ts";
-import { lint } from "./stylelint.ts";
+import { LintResult } from "./type.ts";
 
-const formatter = new EnumType<string>(Object.keys(stylelint.formatters));
-
-function normalizePath(pathLike: string): URL {
-  if (URL.canParse(pathLike)) {
-    return new URL(pathLike);
-  }
-  return toFileUrl(resolve(pathLike));
-}
-
-if (import.meta.main) {
-  const { options, args } = await new Command()
-    .name("stylelint")
-    .type("formatter", formatter)
-    .arguments("<files...:string>")
-    .option("-c, --config <config:string>", "path to configure", {
-      required: true,
-    })
-    .option(
-      "-f, --formatter <formatter:formatter>",
-      "An output formatter",
-      {
-        default: "string",
-      },
-    )
-    .parse(Deno.args);
-
-  const { default: config } = await import(normalizePath(options.config).href);
-  const results = await lint(args, config);
-  console.log(stylelint.formatters[options.formatter]!(results));
+export async function lint(
+  files: string[],
+  config: unknown,
+): Promise<LintResult[]> {
+  const { results } = await stylelint.lint({
+    config,
+    files,
+  });
+  return results;
 }
